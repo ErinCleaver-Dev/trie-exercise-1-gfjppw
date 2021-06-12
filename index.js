d80518d4af2992098d5107ed2e8a4cf2bd5c26d9
ecleaver <37559968+ecleaver@users.noreply.github.com>
2021-06-12 07:55:08 -0400

    pushed

--- index.js
+++ index.js
@@ -39,8 +39,28 @@ class Trie {
    * @return {Array} Array of strings that match for prefix
    */
   getAllWords(str = "") {
+    let wordList = [];
 
+    // first checks to see if thier are values in this, and if not it will return an empty array
+    if(this === undefined) {
+      return [];
+      // next checks to see if end is equal to true which makes it a word.
+    } else if(this.end) {
+      wordList.push(str);
+    } else {
+      // then gets the next letter in the for loop, it did not work with the for each loop though
+      for(let letter in this.children){
+        // assigns the letter to the currentChild
+        let currentChild = this.children[letter];
 
+        // then adds it to the loop
+        wordList = wordList.concat(currentChild.getAllWords(str + letter));
+      }
+    }
+
+    
+
+    return wordList
     
   }
 }
@@ -51,5 +71,5 @@ trie.insert("cow");
 trie.insert("cat");
 trie.insert("dog");
 trie.insert("dad");
-console.log(JSON.stringify(trie))
-console.log(trie.getAllWords());
+//console.log(JSON.stringify(trie))
+console.log(JSON.stringify(trie.getAllWords()));
